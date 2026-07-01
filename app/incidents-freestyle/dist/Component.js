552831282a8c28f4760bc7634102aba956b178b9
Yauheni Sirosh <zhenyasirosh@gmail.com>
2026-07-01 14:05:01 +0000

    deploy

---
+++ app/incidents-freestyle/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/UIComponent","incidentsfreestyle/model/models"],(e,t)=>{"use strict";return e.extend("incidentsfreestyle.Component",{metadata:{manifest:"json",interfaces:["sap.ui.core.IAsyncContentCreation"]},init(){e.prototype.init.apply(this,arguments);this.setModel(t.createDeviceModel(),"device");this.getRouter().initialize()}})});
+//# sourceMappingURL=Component.js.map
---
+++ app/incidents-freestyle/dist/controller/Incidents.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],e=>{"use strict";return e.extend("incidentsfreestyle.controller.Incidents",{onInit(){}})});
+//# sourceMappingURL=Incidents.controller.js.map
